88cc6f1a60ee16ee5ff8a251899cfc056ff5739a
dskleingeld <11743287+dskleingeld@users.noreply.github.com>
2020-04-08 17:58:22 +0200

    finishes basic DQN impl for mountain car, implementation is not yet working

--- src/mountain_car/main.py
+++ src/mountain_car/main.py
@@ -3,11 +3,14 @@
 # Q learning in the book, seep age 78
 
 from typing import Tuple
+from copy import deepcopy
+import random
 
 import gym
 import numpy as np
 import keras
 from keras.layers import Dense
+from keras.optimizers import Adam
 #import random
 # #
 # Type: Box(2)
@@ -31,31 +34,48 @@ from keras.layers import Dense
 
 #env 
 
-print("state_space: {}".format(env.observation_space)) 
-print("action space: {}".format(env.action_space))
-
-#env.observation_space.low[0] = car position
-#env.observation_space.low[1] = car velocity
-print(env.observation_space.low)
-print(env.observation_space.high)
+LEARNING_RATE = 0.005
 
 class Memory:
     def __init__(self):
-        self.events = np.empty()
+        self.events = []
     def remember(self, event):
         self.events.append(event)
     def sample(self): #return 50 samples
-        return np.random.choice(self.events, 50) 
+        if len(self.events) >= 50:
+            return random.sample(self.events, (50))
+        else: 
+            return []
 
 class Predictor:
-    def __init__(self, in_dim: Tuple[int], out_dim: Tuple[int]):
+    # Note on data shape: keras expects data in the shape (N, m)
+    # with N the number of training examplesa and the number of 
+    # possible clauses
+    def __init__(self, in_dim: int, out_dim: int):
+        print(f"in_dim: {in_dim}, out_dim: {out_dim}")
         self.model = keras.Sequential([
-            Dense(input_shape=in_dim),
-            Dense(24, activation='relu'),
+            Dense(24, activation='relu', input_shape=(in_dim,)),
             Dense(48, activation='relu'),
-            Dense(24, activation='relu')
+            Dense(24, activation='relu'),
             Dense(out_dim)
         ])
+        self.model.compile(
+            loss="mean_squared_error",
+            optimizer=Adam(lr=LEARNING_RATE)
+        )
+        self.in_dim = in_dim
+        self.out_dim = out_dim
+
+    #predict effect of each action
+    def predict(self, state):
+        state = state.reshape((1,self.in_dim)) #1 case with 2 inputs
+        res = self.model.predict(state)[0] #unpack list of lists as we have only one case
+        return res
+
+    def fit(self, state, predicted_actions):
+        state = state.reshape((1,self.in_dim)) #1 state with 2 inputs
+        predicted_actions = predicted_actions.reshape((1,self.out_dim)) #1 prediction with 3 actions
+        self.model.fit(state, predicted_actions, verbose=0)
 
 #TODO implement DQN with infrequent weight updates (see page 192 and second blog post above)
 
@@ -75,27 +95,50 @@ class Predictor:
 # - remember the old state, new state, current reward and the action that brought us there
 # - train the neural net on everything in the network, effectively updating the "Q table" that is the neural net.
 
+def replay_and_train(memory, model):
+    gamma = 0.85 #look into gamma
+    training_model = deepcopy(model)
+    # TODO rewrite to do fit all in one pass using data shape 
+    # instead of multiple passes in the for loop
+    for before, action, after, score, game_ended in memory.sample():
+        predicted_effects = model.predict(before) #returns the tree actions
+        if game_ended: #set the correct value for the taken action
+            predicted_effects[action] = score
+        else:
+            predicted_future_effects = model.predict(after)
+            predicted_effects[action] = score+ gamma*max(predicted_future_effects)
+        #print(f"before: {before}, predicted_effects: {predicted_effects}")
+        training_model.fit(before, predicted_effects)
+    model = training_model #replaces the model with the newly trained one
 
-def best_action(state: np.ndarray, model: Predictor) -> int:
+def best_action(state: np.ndarray, model: Predictor, env) -> int:
     epsilon = 0.95 #TODO look into epsilon decay
     if np.random.random() < epsilon:
+        return env.action_space.sample()
+    else:
+        return np.argmax(model.predict(state))
 
 def main():
     env = gym.make("MountainCar-v0")
 
-    env_state_dim = env.observation_space.shape
+    env_state_dim = env.observation_space.shape[0]
     env_action_dim = (env.action_space.__dict__["n"])
     model = Predictor(env_state_dim, env_action_dim)
     memory = Memory()
 
     for training_session in range(200):
-        state = env.reset()
+        before = env.reset()
 
         for step in range(500):
-
-            action = best_action(state, model)
-
-    env.step() # returns [state: numpy.ndarray, reward: float, done: bool, debug: dict]
+            env.render()
+            action = best_action(before, model, env)
+            after, score, game_over, d = env.step(action) # returns [state: numpy.ndarray, reward: float, done: bool, debug: dict]
+            memory.remember((before, action, after, score, game_over))
+            print(f"score, {score}, action: {action}")
+            
+            if step > 25: #ensure some exploration has been done
+                replay_and_train(memory, model)
+            before = after
 
 
 if __name__ == "__main__":
