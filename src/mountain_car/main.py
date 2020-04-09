9e040dfc181be1085d86e9d4238c9fbe0a0b4bbb
dskleingeld <11743287+dskleingeld@users.noreply.github.com>
2020-04-09 18:27:13 +0200

    adds epsilon decay

--- src/mountain_car/main.py
+++ src/mountain_car/main.py
@@ -1,5 +1,4 @@
-# https://towardsdatascience.com/getting-started-with-reinforcement-learning-and-open-ai-gym-c289aca874f
-# https://towardsdatascience.com/reinforcement-learning-w-keras-openai-dqns-1eed3a5338c
+# https://github.com/pylSER/Deep-Reinforcement-learning-Mountain-Car
 # Q learning in the book, seep age 78
 
 from typing import Tuple
@@ -41,11 +40,21 @@ class Memory:
         self.events = []
     def remember(self, event):
         self.events.append(event)
-    def sample(self): #return 50 samples
-        if len(self.events) >= 50:
-            return random.sample(self.events, (50))
+    def sample(self, size=50): #return 50 samples
+        if len(self.events) >= size:
+            return random.sample(self.events, (size))
         else: 
             return []
+
+class Epsilon:
+    def __init__(self, start=1.0, min=0.1, decay=0.1):
+        self.value = start
+        self.min = min
+        self.decay = decay
+    def __lt__(self, other: float) -> bool:
+        return self.value < other
+    def decay(self):
+        self.value = min(self.value-self.decay, self.min)
 
 class Predictor:
     # Note on data shape: keras expects data in the shape (N, m)
@@ -65,6 +74,11 @@ class Predictor:
         )
         self.in_dim = in_dim
         self.out_dim = out_dim
+
+    def clone(self):
+        clone = Predictor(self.in_dim, self.out_dim)
+        clone.set_weights(self.model.get_weights())
+        return clone
 
     #predict effect of each action
     def predict(self, state):
@@ -95,51 +109,78 @@ class Predictor:
 # - remember the old state, new state, current reward and the action that brought us there
 # - train the neural net on everything in the network, effectively updating the "Q table" that is the neural net.
 
-def replay_and_train(memory, model):
+def replay_and_train(memory, model, sample_size):
     gamma = 0.85 #look into gamma
-    training_model = deepcopy(model)
+
+    data_before = np.empty((sample_size, model.in_dim))
+    data_predicted_effects = np.empty((sample_size, model.out_dim))
     # TODO rewrite to do fit all in one pass using data shape 
     # instead of multiple passes in the for loop
-    for before, action, after, score, game_ended in memory.sample():
+    # TODO rewrite further to also bundle the predicts 
+    for i, (before, action, after, score, game_ended) in enumerate(memory.sample(size=sample_size)):
         predicted_effects = model.predict(before) #returns the tree actions
         if game_ended: #set the correct value for the taken action
+            #print("actually won once")
             predicted_effects[action] = score
         else:
             predicted_future_effects = model.predict(after)
             predicted_effects[action] = score+ gamma*max(predicted_future_effects)
         #print(f"before: {before}, predicted_effects: {predicted_effects}")
-        training_model.fit(before, predicted_effects)
-    model = training_model #replaces the model with the newly trained one
+        data_before[i] = before
+        data_predicted_effects[i] = predicted_effects
+    model.model.fit(data_before, data_predicted_effects, verbose=0)
 
-def best_action(state: np.ndarray, model: Predictor, env) -> int:
-    epsilon = 0.95 #TODO look into epsilon decay
+def best_action(state: np.ndarray, model: Predictor, env, epsilon: Epsilon) -> int:
     if np.random.random() < epsilon:
         return env.action_space.sample()
     else:
         return np.argmax(model.predict(state))
 
+
 def main():
+    MAX_STEPS = 200
+    SAMPLE_SIZE = 30
+    
     env = gym.make("MountainCar-v0")
+    env._max_episode_steps = MAX_STEPS+1
 
     env_state_dim = env.observation_space.shape[0]
     env_action_dim = (env.action_space.__dict__["n"])
     model = Predictor(env_state_dim, env_action_dim)
+    model_train = model.clone()
     memory = Memory()
+    epsilon = Epsilon()
 
-    for training_session in range(200):
+    for training_session in range(1000):
         before = env.reset()
 
-        for step in range(500):
+        if training_session % 50 ==0:
             env.render()
-            action = best_action(before, model, env)
-            after, score, game_over, d = env.step(action) # returns [state: numpy.ndarray, reward: float, done: bool, debug: dict]
-            memory.remember((before, action, after, score, game_over))
-            print(f"score, {score}, action: {action}")
-            
-            if step > 25: #ensure some exploration has been done
-                replay_and_train(memory, model)
-            before = after
 
+        for step in range(MAX_STEPS):
+            #if training_session % 10 == 0:
+            #    env.render()
+            action = best_action(before, model, env, epsilon)
+            after, score, success, debug = env.step(action) # returns [state: numpy.ndarray, reward: float, done: bool, debug: dict]
+
+            if success:
+                epsilon.decay()
+                score = 10
+
+            memory.remember((before, action, after, score, game_over))
+            if step > SAMPLE_SIZE: #ensure some exploration has been done
+                replay_and_train(memory, model, model_train, SAMPLE_SIZE)
+            before = after
+    
+            if success:
+                env.render()
+                print(after, score, game_over, debug)
+                #input("Press Enter to continue...")
+                break
+            
+
+        print(f"training session {training_session} done in {step+1} steps")
+        model.set_weights(model_train.get_weights())
 
 if __name__ == "__main__":
     main()
